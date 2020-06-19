2e27f9a2bf30a056c319e558596aa246cff1bf01
Kim Yewo <kyw017763@gmail.com>
2020-06-19 14:27:12 +0900

    React Docs 자습서: 시간 여행 추가하기 (+ (행, 열)로 각 이동 위치 표시)

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,39 +3,17 @@ import Square from './Square';
 
 // 사각형 9개를 렌더링한다
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true, // 다음 플레이어 지정
-    };
-  }
-  handleClick(i) { // onClick={ () => this.setState({ value: 'X' }) } -> State 끌어올리기 위해 지움
-    const squares = this.state.squares.slice();
-    if (squares[i] !== 'O' && squares[i] !== 'X') {
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({ 
-        squares,
-        xIsNext: !this.state.xIsNext,
-      });
-    }
-  };
   renderSquare(i) {
     return (
-      <Square value={this.state.squares[i]} onClick={ () => this.handleClick(i) } />
+      <Square 
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)} 
+      /> // this.handleClick(i)
     );
   }
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = `Winner: ${calculateWinner(this.state.squares)}`;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -54,26 +32,6 @@ class Board extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,19 +3,107 @@ import Board from './Board';
 
 // 게임판을 렌더링하며 나중에 수정할 자리 표시자 값을 가지고 있다.
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+        log: 'Start',
+      }],
+      stepNumber: 0,
+      xIsNext: true, // 다음 플레이어 지정
+      lastPosition: null,
+      log: 'Start',
+    };
+  }
+
+  handleClick(i) { // onClick={ () => this.setState({ value: 'X' }) } -> State 끌어올리기 위해 지움
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); // 만약 과거로 돌아간다면 미래의 기록을 날린다.
+    const curr = history[history.length - 1];
+    const squares = curr.squares.slice();
+    if (calculateWinner(squares) || squares[i]) { // 승부 결과가 null이거나 
+      return;
+    }
+    if (squares[i] !== 'O' && squares[i] !== 'X') { // 모든 칸이 O거나 X면 경기 종료다.
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{ // 불변
+          squares,
+          log: `(${i % 3}, ${Math.floor(i / 3)})`,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0, // step이 짝수면 X 순서
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const curr = history[this.state.stepNumber];
+    const winner = calculateWinner(curr.squares);
+    
+    const moves = history.map((_, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li key={move}>
+          <label>
+            {this.state.history[move].log}&nbsp;&nbsp;
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </label>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={curr.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+        <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
+        <div className="status">{history.map(elem => elem.log).join(' -> ')}</div>
       </div>
     );
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default Game;
+
